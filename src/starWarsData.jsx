3ffd467f150868bac95c107afd0e2e2bc9b368b8
adnansyed101 <sirgeant10@gmail.com>
2023-03-11 00:26:42 +0600

    add data structure

---
+++ src/starWarsData.jsx
@@ -0,0 +1,49 @@
+import ahsokaTano from "./images/ahsoka-tano.jpg";
+
+export default [
+  { id: "Ahsoka Tano", img: "./src/images/ahsoka-tano.jpg" },
+  {
+    id: "Count Dooku",
+    img: "./src/images/count-dooku.jpg",
+  },
+  {
+    id: "Darth Maul",
+    img: "./src/images/darth-maul.jpg",
+  },
+  {
+    id: "Darth Vader",
+    img: "./src/images/darth-vader.jpg",
+  },
+  {
+    id: "General Grievous",
+    img: "./src/images/general-grievous.jpg",
+  },
+  {
+    id: "Han Solo",
+    img: "./src/images/han-solo.jpg",
+  },
+  {
+    id: "Princess Leia",
+    img: "./src/images/leia.jpg",
+  },
+  {
+    id: "Luke Skywalker",
+    img: "./src/images/luke-skywalker.jpg",
+  },
+  {
+    id: "Mace Windu",
+    img: "./src/images/mace-windu.jpg",
+  },
+  {
+    id: "Obi-Wan Kenobi",
+    img: "./src/images/obi-wan.jpg",
+  },
+  {
+    id: "R2-D2",
+    img: "./src/images/r2-d2.jpg",
+  },
+  {
+    id: "Yoda",
+    img: "./src/images/yoda.jpg",
+  },
+];
