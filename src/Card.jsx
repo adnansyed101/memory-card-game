03c65f31ff770f0c1c76af5d1565ae56ecf2a76d
adnansyed101 <sirgeant10@gmail.com>
2023-03-14 23:23:52 +0600

    add animation to cards

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,6 @@
 function Card({ char }) {
   return (
-    <div className="overflow-hidden rounded bg-white shadow">
+    <div className="overflow-hidden rounded bg-white shadow duration-200 ease-out hover:scale-105">
       <img src={char.img} alt="character" className="h-4/5 w-60 object-cover" />
       <p className="my-5 text-center text-xl">{char.id}</p>
     </div>
