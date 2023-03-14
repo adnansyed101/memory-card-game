aa3f1fdcffa375286de84e4cb8d123a86053a51f
adnansyed101 <sirgeant10@gmail.com>
2023-03-14 21:26:43 +0600

    add more styling

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,8 @@
-function Card() {
+function Card({ char }) {
   return (
     <div className="bg-white w-60 rounded shadow overflow-hidden">
-      <img
-        src="./src/images/ahsoka-tano.jpg"
-        alt="character"
-        className=" object-cover "
-      />
-      <p className="text-center text-2xl">Ahsoka</p>
+      <img src={char.img} alt="character" className=" object-cover h-96 " />
+      <p className="text-center text-2xl my-5">{char.id}</p>
     </div>
   );
 }
