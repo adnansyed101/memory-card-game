6128bdc15886ff31255cdc4cece87730ed2502fd
adnansyed101 <sirgeant10@gmail.com>
2023-03-14 21:26:15 +0600

    display cards with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import starWarsData from "./starWarsData";
+import Card from "./Card";
+
 function App() {
+  const [chars, setChars] = useState(starWarsData);
+
+  const cardElements = chars.map((char) => <Card char={char} />);
+
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="container mx-auto">
+      <h1 className="text-center text-4xl my-4">Memory Card Game</h1>
+      <div className="grid grid-cols-6 gap-10 my-10">{cardElements}</div>
     </div>
   );
 }
